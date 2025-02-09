69182db3a11e045f3c6df223875f7a254d1a47e5
tago893 <varun.chikkala2002@gmail.com>
2025-02-09 15:58:56 -0800

    final code edits

--- src_main/app.py
+++ src_main/app.py
@@ -1,10 +1,12 @@
 import flask
+from utils.api_key_generation import validate_api_key
+from functools import wraps
 from app.dashboard import Dashboard
 from app.index import Index
 from app.login import Login
 from app.logout import Logout
 from app.signup import Signup
-
+from flask import jsonify 
 
 import os
 app = flask.Flask(__name__, template_folder='static/templates')       
@@ -30,5 +32,20 @@ app.add_url_rule('/logout',
                  view_func=Logout.as_view('logout'),
                  methods=["GET"])
 
+# API Key decorator using you1r validation function
+@app.route("/hello/<apikey>/", methods=["GET"])
+def hello_world(apikey):
+    """Mock API that requires API key validation."""
+    if (require_api_key(apikey)):
+        return jsonify({"message": "Hello, World!", "status": "Success"}), 200  # Success
+
+
+def require_api_key(api_key):
+    if not api_key:
+        return jsonify({"error": "Missing API key"}), 400  # Bad Request
+
+    # Validate API Key (Using function from api_key_generation.py)
+    if not validate_api_key(api_key):
+        return jsonify({"error": "Unauthorized. Invalid API key"}), 401  # Unauthorized
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
